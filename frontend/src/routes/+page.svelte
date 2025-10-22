ae862840d86ad0643ec24a41124c2dfe1a7e3d36
RedenHilal <hilalrahmatullah11@gmail.com>
2025-10-22 20:25:46 +0700

    ini branchku, jangan di otak-atik ya :3

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,24 @@
 <h1 class="underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="underline font-bold ">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="bg-white w-100 h-100" bind:this={c}>
+</div>
+
+<style lang="postcss">
+@reference "tailwindcss";
+
+:global(html) {
+	background-color: theme(--color-gray-500);
+}
+
+</style>
+
+<script>
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
+	let c;
+
+	onMount(()=>{
+		gsap.to(c, {x:200, rotation:240, duration:2})
+	});
+</script>
