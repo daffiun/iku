52bbda0fc42c5305cdc77a9c06ea565be24e1ec5
IrvanKurniawan <irvankurniawan624@gmail.com>
2025-10-27 20:30:24 +0700

    feat(fe): Add Alert Contact Us

--- frontend/src/lib/components/Contact.svelte
+++ frontend/src/lib/components/Contact.svelte
@@ -10,6 +10,7 @@
         { text: 'Lainnya', val_ID: 'lainnya', }
     ];
 
+    let showAlert = false;
     let type = 'individual';
     let lainnyaTypeInput = ''; 
     
@@ -18,13 +19,36 @@
     function selectPill(val_ID) {
         type = val_ID;
     }
+
+    function handleSubmit(event) {
+        event.preventDefault(); 
+        showAlert = true; 
+    }
 </script>
 
 <section id="contact_us" class="flex-center flex-col mt-12">
-    <div class="title text-center text-5xl">
+    <div class="title text-center text-5xl mb-12">
         <h2>Memiliki <span class="text-base">Pertanyaan?</span><br>Hubungi Kami.</h2>
     </div>
-    <form action="" class="grid grid-cols-2 gap-x-14 gap-y-8 max-w-6xl mt-14">
+    {#if showAlert}
+        <div class="bg-teal-50 border-t-2 border-teal-500 mb-6 rounded-lg p-4 max-w-2xl" role="alert" tabindex="-1" aria-labelledby="hs-bordered-success-style-label" transition:fly={{ x: -200, duration: 500, easing: quintOut }}>
+            <div class="flex">
+                <div class="shrink-0">
+                    <span class="inline-flex justify-center items-center size-8 rounded-full border-4 border-teal-100 bg-teal-200 text-teal-800">
+                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path>
+                            <path d="m9 12 2 2 4-4"></path>
+                        </svg>
+                    </span>
+                </div>
+                <div class="ms-3">
+                    <h3 id="hs-bordered-success-style-label" class="text-gray-800 font-semibold">Terimakasih telah mengirimkan pesan.</h3>
+                    <p class="text-sm text-gray-700">Pesan Anda telah kami terima dan akan segera diproses oleh tim kami. Kami akan menghubungi Anda kembali dalam waktu 1-2 hari kerja.</p>
+                </div>
+            </div>
+        </div>
+    {/if}
+    <form on:submit={handleSubmit} class="grid grid-cols-2 gap-x-14 gap-y-8 w-2xl max-w-6xl">
         <label for="nama" class="col-span-2">
             <input type="text" id="nama" name="nama" class="pr-5" placeholder="Nama" required aria-label="Nama">
             <span class="required"></span>
