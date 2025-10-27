4919b4ee48e3fb8360eb260ccc4b5386628ab45e
IrvanKurniawan <irvankurniawan624@gmail.com>
2025-10-27 12:22:59 +0700

    feat(fe): Add Testing Header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
-	import "../app.css";
+    import "../app.css";
+    import Header from '$lib/components/Header.svelte'; 
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href="/favicon/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
 </svelte:head>
 
-{@render children?.()}
+<Header />
+
+<main class="flex flex-col min-h-screen">
+    <div class="grow">
+        {@render children?.()}
+    </div>
+</main>
